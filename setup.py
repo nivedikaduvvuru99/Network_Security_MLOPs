35934573dc716fe6d2ecb5c550642e6008113f3a
nivedikaduvvuru99 <nividl99@gmail.com>
2025-04-03 22:49:50 +0800

    requirements added

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import find_packages,setup  #find packages check for __init__ folder
+from typing import List
+
+ #networksecurity.egg-info is a metadata file
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+    
+    try:
+        # Open and read the requirements.txt file
+        with open('requirements.txt', 'r') as file:
+            # Read lines from the file
+            lines = file.readlines()
+            # Process each line
+            for line in lines:
+                # Strip whitespace and newline characters
+                requirement = line.strip()
+                # Ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    
+        
+    return requirement_list
+print(get_requirements())
+setup(
+    name="networksecurity",
+    version="0.0.1",
+    author="Nivedika D L",
+    author_email="nividl99@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements()
+)
+
+
+
+
+
+
+
+
+
+
+
+
+ #find packages check for __init__ folder
+ #networksecurity.egg-info is a metadata file
